2b3185f7b160e8db88eab12d7d74cbf6d9aa9fb2
sgsuper12373 <152173682+sgsuper12373@users.noreply.github.com>
2024-10-30 23:23:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 #Verifying Monty Hall's Theorem
 
 import random
-a=random.randing(1,3)
-guess=int(input('enter room number you want to bet'))
+a=random.randint(1,3)
+guess=int(input('enter Door number you want to bet : '))
 
-#Monties unrevealing door
+#Deciding which Door to reveal
 doors=[1,2,3]
 doors.remove(guess)
+if a!=doors[0] :
+    print("oops there is nothing behind ",doors[0], " Door ")
+    doors.pop(0)
+    
+else :
+    print("oops there is nothing behind this door",doors[1]," Door ") 
+    doors.pop(1)
+    
 
-
-
-
-
-#giving second chance to recheack doors
-secondChance=input("Do you want to swap dooers? y/n")
+#Monty asking to swap doors
+choice=input("Do you Want to change choice of Door? Y/N  : ")
 
 #swapping doors if agreed to swap
-if secondChance=='y' or secondChance=="Y":
-    guess=doors[(doors.index(guess)+1)%2]
+if choice=='y' or choice=="Y":
+    guess=doors[0]
 
 if guess==a:
     print("you Won!!")
